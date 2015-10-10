9589df95d95e8b26662bd1f375b26c5ca9386216
Sinclair-Emmanuel Smith <esmith@projectaxil.us>
2015-10-10 21:29:47 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='swiss',
+      version='0.0.0',
+      description='Creating a pocket sized toolkit library for any situation.',
+      author='Mr Axilus',
+      author_email='esmith@projectaxil.us',
+      url='http://swiss.projectaxil.us',
+      packages=['swiss'],
+     )
+
